23e5f51fa9255cc7682da35ff4dc105498d42209
RodrigoYujiKogaKuroda <yujikuroda.rykk@gmail.com>
2023-04-17 12:09:15 -0300

    chore: change .eslintrc to ignore import/export rule

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,5 +2,4 @@ export * from './users-service';
 export * from './authentication-service';
 export * from './events-service';
 export * from './enrollments-service';
-// eslint-disable-next-line import/export
 export * from './tickets-service';
