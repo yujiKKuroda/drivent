ecd2675344f1ab604f80216f3f450f0faa11ca88
RodrigoYujiKogaKuroda <yujikuroda.rykk@gmail.com>
2023-04-18 17:45:47 -0300

    chore: remove unused function call

--- src/middlewares/error-handling-middleware.ts
+++ src/middlewares/error-handling-middleware.ts
@@ -1,13 +1,8 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import { ApplicationError } from '@/protocols';
 
-export function handleApplicationErrors(
-  err: ApplicationError | Error,
-  _req: Request,
-  res: Response,
-  _next: NextFunction,
-) {
+export function handleApplicationErrors(err: ApplicationError | Error, _req: Request, res: Response) {
   if (err.name === 'CannotEnrollBeforeStartDateError') {
     return res.status(httpStatus.BAD_REQUEST).send({
       message: err.message,
