68dc2066fa3bd5b472b53145a33d001739e41736
RodrigoYujiKogaKuroda <yujikuroda.rykk@gmail.com>
2023-05-02 13:48:14 -0300

    fix: fix type of element returned in booking controller

--- src/controllers/booking-controller.ts
+++ src/controllers/booking-controller.ts
@@ -8,7 +8,6 @@ export async function getBooking(req: AuthenticatedRequest, res: Response) {
 
   try {
     const booking = await bookingService.getBookingByUserId(userId);
-
     return res.status(httpStatus.OK).send(booking);
   } catch (error) {
     if (error.name === 'NotFoundError') {
@@ -18,9 +17,12 @@ export async function getBooking(req: AuthenticatedRequest, res: Response) {
 }
 
 export async function createBooking(req: AuthenticatedRequest, res: Response) {
+  const { roomId } = req.body;
+  const { userId } = req;
+
   try {
-    const booking = await bookingService.createBookingForUser(req.body.roomId, req.userId);
-    return res.status(httpStatus.OK).send(booking.id);
+    const booking = await bookingService.createBookingForUser(roomId, userId);
+    return res.status(httpStatus.OK).send(String(booking.id));
   } catch (error) {
     if (error.name === 'NotFoundError') {
       return res.sendStatus(httpStatus.NOT_FOUND);
@@ -36,7 +38,7 @@ export async function changeBooking(req: AuthenticatedRequest, res: Response) {
 
   try {
     const booking = await bookingService.changeBookingForUser(roomId, bookingId);
-    return res.status(httpStatus.OK).send(booking.id);
+    return res.status(httpStatus.OK).send(String(booking.id));
   } catch (error) {
     if (error.name === 'NotFoundError') {
       return res.sendStatus(httpStatus.NOT_FOUND);
